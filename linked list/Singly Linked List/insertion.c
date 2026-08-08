a0021e38713edb58fdb41cfcaa79ae3b335e3313
shivakshat <shivakshatgandhi@gmail.com>
2026-08-09 00:42:40 +0530

    Add new changes

--- linked list/Singly Linked List/insertion.c
+++ linked list/Singly Linked List/insertion.c
@@ -27,15 +27,50 @@ struct Node* creation(){
     free(temp);
     return head;
 }
+void insertAtBeginning(){
+    newnode = (struct Node*)malloc(sizeof(struct Node));
+    printf("Enter the value for new first node: ");
+    scanf("%d",&newnode->data);
+    newnode->next = head;
+    head = newnode;
+}
+void insertAtEnd(){
+    newnode = (struct Node*)malloc(sizeof(struct Node));
+    printf("Enter the value for new end node: ");
+    scanf("%d",&newnode->data);
+    temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newnode;
+}
+void insertInBetween(){
+    newnode = (struct Node*)malloc(sizeof(struct Node));
+    int pos;
+    printf("Enter the position: ");
+    scanf("%d",&pos);
+    printf("Enter the value for new node: ");
+    scanf("%d",&newnode->data);
+    temp = head;
+    for(int i=1;i<pos-1;i++){
+        temp = temp->next;
+    }
+    newnode->next = temp->next;
+    temp->next = newnode;
+}
 void access(){
     temp = head;
     while(temp != NULL){
         printf("%d ",temp->data);
         temp = temp->next;
     }
+    free(temp);
     return;
 }
 void main(){
     creation();
+    // insertAtBeginning();
+    // insertAtEnd();
+    insertInBetween();
     access();
 }
